560f52017d3e9e29e22db2f70b525b7fa3957688
Ernesto del valle <erni.delvalle@outlook.com>
2022-08-06 11:30:14 +0200

    add api calls

---
+++ src/app/api/api.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Employee } from './models/employee';
+import { map, Observable } from 'rxjs';
+
+const ENDPOINTS = {
+  EMPLOYEES: '/api/v2/employees',
+  DEPARTMENTS: '/api/v2/departments',
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(ENDPOINTS.EMPLOYEES)
+      .pipe(
+        map(employees => {
+          employees.forEach(employee => {
+            employee.dateOfBirth = new Date(employee.dateOfBirth);
+            employee.dateOfHire = new Date(employee.dateOfHire);
+          });
+          return employees;
+        }),
+      );
+  }
+
+  public getDepartments(): Observable<string[]> {
+    return this.http.get<string[]>(ENDPOINTS.DEPARTMENTS);
+  }
+
+}
---
+++ src/app/api/models/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+    avatar: string;
+    dateOfBirth: Date;
+    dateOfHire: Date;
+    department: string;
+    firstName: string;
+    id: number;
+    lastName: string;
+    married: boolean;
+    position: string;
+}
